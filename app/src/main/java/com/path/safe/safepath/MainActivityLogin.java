14eebef4c534448573c61ddc3b88eaed83598fef
joselhuillca <joselhuillca@gmail.com>
2017-12-10 00:29:34 +0000

    Insertanto NavigationDrawer

--- app/src/main/java/com/path/safe/safepath/MainActivityLogin.java
+++ app/src/main/java/com/path/safe/safepath/MainActivityLogin.java
@@ -1,9 +1,11 @@
 package com.path.safe.safepath;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +26,7 @@ public class MainActivityLogin extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         this.InitComponents();
+        this.ActionsComponents();
     }
 
     private void InitComponents(){
@@ -31,5 +34,31 @@ public class MainActivityLogin extends AppCompatActivity {
         edt_email = (EditText)findViewById(R.id.email);
         edt_passw = (EditText)findViewById(R.id.password);
         btn_singIn = (Button)findViewById(R.id.buttonsingin);
+    }
+
+    private void ActionsComponents(){
+        btn_singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                funSingIn();
+            }
+        });
+    }
+
+    //Funcion de Iniciar Sesion mediante la cuenta de Safe Path
+    public  void funSingIn() {
+        //
+        this.Ir_a_mapaGeneral();
+    }
+
+    //Ir a MapaGeneral una vez iniciado sesion
+    public  void Ir_a_mapaGeneral()
+    {
+        //Te manda al NavigationDrawerActivity
+        Intent intent = new Intent(this,NavigationDrawerActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.my_fade_in,R.anim.my_fade_out);
+        finish();
+
     }
 }
